712ad518af68ba80e6beee47dc847bf3a0b3b8ac
Matthiea <lambreschim@gmail.com>
2024-02-10 12:25:37 +0100

    V 0.0.1

--- src/lib/dht.rs
+++ src/lib/dht.rs
@@ -4,12 +4,13 @@ pub mod dht11 {
         thread::sleep,
         time::{Duration, Instant},
     };
+
     use sysfs_gpio::{Direction, Pin};
 
     pub fn get_data(pin_num: u64) -> Option<[f32; 2]> {
         let pin = Pin::new(pin_num);
-        let data: [f32; 2];
-        let raw: Vec<bool>;
+        let mut data: [f32; 2] = Default::default();
+        let mut raw: Vec<bool> = Vec::new();
 
         //initialization of the comunication
         pin.with_exported(|| {
@@ -27,11 +28,19 @@ pub mod dht11 {
                 sleep(Duration::from_micros(160));
                 raw = get_bit(pin);
                 data = IEEE_754(raw);
-                return Some(data);
+                return Ok(());
             } else {
-                return None;
+                return Err(sysfs_gpio::Error::Unexpected(
+                    "Errore nella lettura dei dati".to_string(),
+                ));
             }
         });
+
+        if !data.is_empty() {
+            Some(data)
+        } else {
+            None
+        }
     }
 
     // function used for get the two f32
@@ -43,7 +52,7 @@ pub mod dht11 {
     }
 
     fn get_bit(pin: Pin) -> Vec<bool> {
-        let mut bit: Vec<bool>;
+        let mut bit: Vec<bool> = Vec::new();
 
         while bit.len() <= 40 {
             let start = Instant::now();
