64b4e2c7b65276bb34a1d47199b495c6a453a4af
Matthiea <lambreschim@gmail.com>
2024-02-18 21:06:27 +0100

    Imporved the DHT file

--- src/lib/dht.rs
+++ src/lib/dht.rs
@@ -1,47 +1,60 @@
 #![allow(unused)]
 
-pub mod dht11 {
+pub mod dht {
 
-    use std::{
-        thread::sleep,
-        time::{Duration, Instant},
-    };
+    use std::{thread::sleep, time::Duration};
 
-    use sysfs_gpio::{Direction, Error, Pin};
+    use sysfs_gpio::{Direction, Pin};
+
+    pub struct Dht11 {
+        pub pin: u8,
+    }
+
+    pub trait Define {
+        fn new(pin_number: u8) -> Self;
+    }
+
+    impl Define for Dht11 {
+        fn new(pin_number: u8) -> Self {
+            Dht11 { pin: pin_number }
+        }
+    }
 
     pub fn get_data(pin_num: u64) -> Option<[f32; 2]> {
         let pin = Pin::new(pin_num);
+
         let mut data: [f32; 2] = Default::default();
         let mut raw: Vec<bool> = Vec::new();
 
-        //initialization of the comunication
-        pin.with_exported(|| {
-            pin.set_direction(Direction::Out).unwrap();
-
+        // Initialization of the communication
+        if let Ok(_) = pin.with_exported(|| {
+            pin.set_direction(Direction::Out)?;
             pin.set_value(0);
             sleep(Duration::from_millis(20));
             pin.set_value(1);
             sleep(Duration::from_micros(40));
+            pin.set_direction(Direction::In)?;
 
-            pin.set_direction(Direction::In).unwrap();
-
-            if pin.get_value().unwrap() == 0 {
+            if pin.get_value()? == 0 {
                 sleep(Duration::from_micros(160));
                 raw = get_bit(pin);
-                data = IEEE_754(raw);
+                data = ieee_754(raw);
             }
-            return Ok(());
-        });
 
-        if !data.is_empty() {
-            Some(data)
+            Ok(())
+        }) {
+            if !data.is_empty() {
+                Some(data)
+            } else {
+                None
+            }
         } else {
             None
         }
     }
 
-    // function used for get the two f32
-    fn IEEE_754(mut bits: Vec<bool>) -> [f32; 2] {
+    // Function used to get the two f32
+    fn ieee_754(mut bits: Vec<bool>) -> [f32; 2] {
         let mut temp: f32 = 0.0;
         let mut umidy: f32 = 0.0;
 
@@ -52,7 +65,6 @@ pub mod dht11 {
                 for _i in 0..16 {
                     temp += match bits[0] {
                         true => f32::powi(2.0, y),
-
                         false => 0.0,
                     };
 
@@ -63,12 +75,10 @@ pub mod dht11 {
                 for _i in 0..16 {
                     umidy += match bits[0] {
                         true => f32::powi(2.0, y),
-
                         false => 0.0,
                     };
 
                     y -= 1;
-
                     bits.remove(0);
                 }
             } else {
@@ -76,7 +86,7 @@ pub mod dht11 {
             }
         }
 
-        return [temp, umidy];
+        [temp, umidy]
     }
 
     fn get_bit(pin: Pin) -> Vec<bool> {
@@ -85,10 +95,10 @@ pub mod dht11 {
         while bit.len() <= 40 {
             sleep(Duration::from_micros(50));
 
-            if pin.get_value().unwrap() == 1 {
+            if pin.get_value().unwrap_or_default() == 1 {
                 sleep(Duration::from_micros(50));
 
-                if pin.get_value().unwrap() == 0 {
+                if pin.get_value().unwrap_or_default() == 0 {
                     bit.push(true);
                 } else {
                     bit.push(false);
@@ -96,6 +106,7 @@ pub mod dht11 {
             }
             sleep(Duration::from_micros(20));
         }
-        return bit;
+
+        bit
     }
 }
